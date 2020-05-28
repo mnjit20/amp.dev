cea52447a4a336eab3ec4cb223566160001945b5
Robin Van Opstal <robin.van.opstal@mhmk.de>
2020-05-28 12:52:46 +0200

    💄 Error-list styling

--- playground/src/error-list/error-list.js
+++ playground/src/error-list/error-list.js
@@ -63,13 +63,13 @@ class ErrorList {
       /* eslint-disable max-len */
       this.container.innerHTML = `
         <div class="title">
+        <h2>Validation Result</h2>
         <button class="button close">
           <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
             <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
             <path d="M0 0h24v24H0z" fill="none"/>
           </svg>
         </button>
-        <h2>Validation Result</h2>
         </div>
         <ul>${validationResult.errors.map(this.renderError).join('')}</ul>
         `;
@@ -83,7 +83,7 @@ class ErrorList {
   renderError(error, index) {
     return `<li class="validation-error ${error.icon}" data-index="${index}">
             <div>
-              <div class="message">${error.message}</div>
+              <p class="message">${error.message}</p>
               <div class="category">${error.category}</div>
               <div class="location">line ${error.line}, column ${error.col}</div>
             </div>
